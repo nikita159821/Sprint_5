3f2cf49fd8d7d3771e371d6075d33d25ebce6d28
nikita <tele2.rumerkulov@yandex.ru>
2024-04-05 16:02:53 +0500

    first commit

---
+++ tests/test_new_register_page.py
@@ -0,0 +1,17 @@
+from pages.register_page import RegisterPage
+
+
+def test_register_page(browser):
+    # создал объект класса
+    register_page = RegisterPage(browser)
+    # Открываем страницу регистрации
+    register_page.open()
+
+    # Вводим все данные
+    register_page.send_keys_register_button()
+    # Нажимаем на кнопку "Регистрация"
+    register_page.click_register_button()
+
+    # После авториации появляется форма фхода. Проверяем тайтл "Вход", если есть,
+    # значит тест пройден, регистрация завершена"
+    assert register_page.title_authorization().is_displayed()
