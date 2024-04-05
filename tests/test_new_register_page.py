e1522688ad82627bbadc8db617b106d41d6c5e51
nikita <tele2.rumerkulov@yandex.ru>
2024-04-05 17:30:21 +0500

    first commit

--- tests/test_new_register_page.py
+++ tests/test_new_register_page.py
@@ -15,3 +15,18 @@ def test_register_page(browser):
     # После авториации появляется форма фхода. Проверяем тайтл "Вход", если есть,
     # значит тест пройден, регистрация завершена"
     assert register_page.title_authorization().is_displayed()
+
+
+def test_register_error_password(browser):
+    # создал объект класса
+    register_page = RegisterPage(browser)
+    # Открываем страницу регистрации
+    register_page.open()
+
+    # Вводим все данные
+    register_page.send_keys_register_error_button()
+    # Нажимаем на кнопку "Регистрация"
+    register_page.click_register_button()
+
+    # Сравнимаем полученный текст с ожидаемым.
+    assert register_page.password_error_text() == 'Некорректный пароль'
