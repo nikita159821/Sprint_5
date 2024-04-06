7cb56a4577b590032676b83c154026b9f80353f4
nikita <tele2.rumerkulov@yandex.ru>
2024-04-06 16:27:00 +0500

    login

--- tests/test_login_to_account.py
+++ tests/test_login_to_account.py
@@ -6,12 +6,11 @@ from pages.login_page import LoginPage
 def test_login_to_account_main_page(browser):
     # создал объект класса
     login_page = LoginPage(browser)
+
     # Открываем страницу регистрации
     login_page.open()
-
     # Нажимаем кнопку "Войти в аккаунт"
     login_page.button_login_click()
-
     # Вводим логин и пароль
     login_page.send_keys_authorization()
     # Нажимаем кнопку "Войти"
