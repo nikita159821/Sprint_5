a7648041822d79d8ac1a0cbef04d4e9f71a19de2
nikita <tele2.rumerkulov@yandex.ru>
2024-04-07 10:25:19 +0500

    test

--- pages/constructor_page.py
+++ pages/constructor_page.py
@@ -2,19 +2,20 @@ from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
 button_constructor_selector = (By.XPATH, "//p[contains(text(),'Конструктор')]")
+logo_constructor_selector = (By.XPATH, "//a[@href='/']")
 
 
 class ConstructorPage(BasePage):
     def __init__(self, browser):
         super().__init__(browser)
 
-    def open_constructor(self):
+    def open_logo_stellar_burgers(self):
         self.browser.get('https://stellarburgers.nomoreparties.site/login')
 
-    # Метод возвращает локатор кнопки "Конструктор"
-    def button_constructor(self):
-        return self.find_element(*button_constructor_selector)
+    # Метод возвращает локатор логотипа "stellar_burgers"
+    def button_constructor_logo(self):
+        return self.find_element(*logo_constructor_selector)
 
     # Метод нажимает кнопку "Конструктор"
     def button_constructor_click(self):
-        self.button_constructor().click()
+        self.button_constructor_logo().click()
---
+++ tests/test_to_constructor_logo.py
@@ -0,0 +1,15 @@
+import time
+from pages.constructor_page import ConstructorPage
+
+
+def test_to_constructor_logo(browser):
+    # создал объект класса
+    login_page = ConstructorPage(browser)
+    # Открываем страницу
+    login_page.open_logo_stellar_burgers()
+
+    # Yажимаем кнопку "Личный Кабинет"
+    login_page.button_constructor_click()
+    time.sleep(15)
+
+    assert browser.current_url == "https://stellarburgers.nomoreparties.site/"
