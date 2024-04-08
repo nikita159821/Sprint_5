7ac0b019db3dde4ddb578eea9f2d106cc8b560c5
nikita <tele2.rumerkulov@yandex.ru>
2024-04-08 10:50:36 +0500

    test

--- pages/constructor_page.py
+++ pages/constructor_page.py
@@ -3,6 +3,13 @@ from pages.base_page import BasePage
 
 button_constructor_selector = (By.XPATH, "//p[contains(text(),'Конструктор')]")
 logo_constructor_selector = (By.XPATH, "//a[@href='/']")
+constructor_burger_selector_1 = (By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[2]")
+constructor_burger_selector_2 = (By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[1]")
+constructor_burger_selector_3 = (By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[3]")
+constructor_burger_selector_4 = (By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[2]/span")
+constructor_burger_selector_5 = (By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[3]/span")
+constructor_burger_selector_6 = (By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[1]/span")
+
 
 
 class ConstructorPage(BasePage):
@@ -15,9 +22,51 @@ class ConstructorPage(BasePage):
     def open_constructor(self):
         self.browser.get('https://stellarburgers.nomoreparties.site/login')
 
+    def open_burger(self):
+        self.browser.get('https://stellarburgers.nomoreparties.site/')
+
     # Метод возвращает локатор логотипа "stellar_burgers"
     def button_constructor_logo(self):
         return self.find_element(*logo_constructor_selector)
+
+    # Метод возвращает локатор "Булки"
+    def construct_burger_buns(self):
+        return self.find_element(*constructor_burger_selector_2)
+
+    # Метод возвращает локатор "Соусы"
+    def constructor_burgers(self):
+        return self.find_element(*constructor_burger_selector_1)
+
+    # Метод возвращает локатор "Начинки"
+    def constructor_burgers_fillings(self):
+        return self.find_element(*constructor_burger_selector_3)
+
+    # Метод нажимает кнопку "Булки"
+    def button_constructor_burgers_buns(self):
+        self.construct_burger_buns().click()
+
+    # Метод нажимает кнопку "Соусы"
+    def button_constructor_burgers(self):
+        self.constructor_burgers().click()
+
+    # Метод нажимает кнопку "Начинки"
+    def button_constructor_burgers_fillings(self):
+        self.constructor_burgers_fillings().click()
+
+    # Метод возвращает локатор текста "Булки"
+    def get_text_buns(self):
+        element = self.find_element(*constructor_burger_selector_6)
+        return element.text  # Возвращаем текст элемента
+
+    # Метод возвращает локатор текста "Соусы"
+    def get_text_sauces(self):
+        element = self.find_element(*constructor_burger_selector_4)
+        return element.text  # Возвращаем текст элемента
+
+    # Метод возвращает локатор текста "Начинки"
+    def get_text_fillings(self):
+        element = self.find_element(*constructor_burger_selector_5)
+        return element.text  # Возвращаем текст элемента
 
     # Метод нажимает кнопку "Конструктор"
     def button_constructor_click(self):
--- tests/test_to_constructor.py
+++ tests/test_to_constructor.py
@@ -8,8 +8,47 @@ def test_personal_account_to_constructor_page(browser):
     # Открываем страницу
     login_page.open_constructor()
 
-    # Yажимаем кнопку "Личный Кабинет"
+    # Нажимаем кнопку "Личный Кабинет"
     login_page.button_constructor_click()
     time.sleep(1)
 
     assert browser.current_url == "https://stellarburgers.nomoreparties.site/"
+
+
+def test_navigation_to_sauces_section(browser):
+    # создал объект класса
+    login_page = ConstructorPage(browser)
+    # Открываем страницу
+    login_page.open_burger()
+
+    # Нажимаем "Соусы"
+    login_page.button_constructor_burgers()
+    time.sleep(3)
+    assert login_page.get_text_sauces() == 'Соусы'
+
+
+def test_navigation_to_fillings_section(browser):
+    # создал объект класса
+    login_page = ConstructorPage(browser)
+    # Открываем страницу
+    login_page.open_burger()
+
+    # Нажимаем "Соусы"
+    login_page.button_constructor_burgers_fillings()
+    time.sleep(3)
+    assert login_page.get_text_fillings() == 'Начинки'
+
+
+def test_navigation_to_buns_section(browser):
+    # создал объект класса
+    login_page = ConstructorPage(browser)
+    # Открываем страницу
+    login_page.open_burger()
+
+    # Нажимаем "Соусы", чтобы потом переключиться на "Булки"
+    login_page.button_constructor_burgers_fillings()
+    time.sleep(3)
+
+    # Нажимаем на "Булки"
+    login_page.button_constructor_burgers_buns()
+    assert login_page.get_text_buns() == 'Булки'
