839ef083aa4ffa5de70a6d48c6b047337b1df70b
nikita <tele2.rumerkulov@yandex.ru>
2024-04-10 12:24:31 +0500

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 import pytest
 
-
 import sys
 import os
 
@@ -9,12 +8,10 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-
-
 @pytest.fixture
 def browser():
     driver_browser = webdriver.Chrome()
     driver_browser.implicitly_wait(10)
     yield driver_browser
-    return driver_browser
+    driver_browser.quit()
 
