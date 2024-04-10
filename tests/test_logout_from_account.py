f71182b3f309f8b99bf108e2d39f3ebdd47029f8
nikita <tele2.rumerkulov@yandex.ru>
2024-04-10 12:47:02 +0500

    fix

--- tests/test_logout_from_account.py
+++ tests/test_logout_from_account.py
@@ -4,23 +4,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_logout_button_in_personal_account(browser):
-    # создал объект класса
-    exit_page = ExitLkPage(browser)
-    # Открываем страницу авторизации
-    exit_page.open()
+class TestBurgerLogout:
+    def test_logout_button_in_personal_account(self,browser):
+        # создал объект класса
+        exit_page = ExitLkPage(browser)
+        # Открываем страницу авторизации
+        exit_page.open()
 
-    # Авторизуемся
-    exit_page.send_keys_authorization()
-    exit_page.button_login_click()
+        # Авторизуемся
+        exit_page.send_keys_authorization()
+        exit_page.button_login_click()
 
-    # Заходим в лк и выходим через кнопку "Выход"
-    exit_page.button_lk_click()
-    exit_page.logout_click()
+        # Заходим в лк и выходим через кнопку "Выход"
+        exit_page.button_lk_click()
+        exit_page.logout_click()
 
-    # Явное ожидание загрузки страницы
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.url_to_be(URL_LOGIN))
+        # Явное ожидание загрузки страницы
+        wait = WebDriverWait(browser, 10)
+        wait.until(EC.url_to_be(URL_LOGIN))
 
-    assert browser.current_url == URL_LOGIN
+        assert browser.current_url == URL_LOGIN
 
